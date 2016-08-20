5663b9d68c531ae6d7464e340c62aa1a20b2f0ca
nickangtc <nickangtc@gmail.com>
2016-08-20 23:23:37 +0800

    Finished game mechanics.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ console.log('is js working?');
 document.addEventListener('DOMContentLoaded', function() {
 
   document.getElementById("start-button").addEventListener("click", startGame);
+  document.getElementById("reset-button").addEventListener("click", resetGame);
 
   document.getElementById("box1").addEventListener("click", function () {
     makeMove("box1");
@@ -42,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var gameStarted = false;
   var round = 1;
   var boardArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var boxNum = {
+  var boxIndex = {
     box1: 1,
     box2: 2,
     box3: 3,
@@ -66,31 +67,46 @@ document.addEventListener('DOMContentLoaded', function() {
   function whoseTurn () {
     if (round % 2 === 0) {
       console.log("Player2's turn");
-      document.getElementById("show-turn").innerHTML = player1.name + ", it's your turn (" + player1.symbol + ")";
-      return player2.symbol;
+      return player2;
     }
     else {
       console.log("Player1's turn");
-      document.getElementById("show-turn").innerHTML = player2.name + ", it's your turn (" + player2.symbol + ")";
-      return player1.symbol;
+      return player1;
     }
+  }
+
+  function lastPlayer () {
+    console.log("checking last player...");
+    if (whoseTurn() === player2) {
+      return player1;
+    }
+    else if (whoseTurn() === player1) {
+      return player2;
+    }
+  }
+
+  function updateTurnDisplay (player) {
+    document.getElementById("show-turn").innerHTML = player.name + ", it's your turn (" + player.symbol + ")";
+    console.log("player name: " + player.name);
   }
 
   function makeMove(boxId) {
     console.log("inside makeMove function with " + boxId);
     console.log("class list of boxId " + document.getElementById(boxId).classList);
     if (gameStarted && document.getElementById(boxId).classList.length === 1) {
-      if (whoseTurn() === "X") {
+      if (whoseTurn().symbol === "X") {
+        updateTurnDisplay(player1);
         console.log("tile changed to X bg");
         document.getElementById(boxId).classList.add("x");
-        boardArray[boxNum[boxId] - 1] = "X";
+        boardArray[boxIndex[boxId] - 1] = "X";
         console.log(boardArray);
       }
-      else if (whoseTurn() === "O") {
+      else if (whoseTurn().symbol === "O") {
+        updateTurnDisplay(player2);
         console.log("tile changed to O bg");
         // document.getElementById(boxId).setAttribute("class", "o-symbol");
         document.getElementById(boxId).classList.add("o");
-        boardArray[boxNum[boxId] - 1] = "O";
+        boardArray[boxIndex[boxId] - 1] = "O";
         console.log(boardArray);
       }
       // document.getElementById(boxId).textContent = whoseTurn();
@@ -103,49 +119,56 @@ document.addEventListener('DOMContentLoaded', function() {
   function winOrLose() {
     if (round === 10) {
       console.log("it's a draw match");
-      alert("And... it's a draw. Click Reset and Start for a rematch!");
+      alert("And... it's a draw. Click RESET and Start for a rematch!");
     }
     else {
       // CHECK ROW 1
       if (boardArray[0] === boardArray[1] && boardArray[0] === boardArray[2]) {
-        console.log("1st row win");
+        alert(lastPlayer().name + " wins!");
       }
       // CHECK ROW 2
       if (boardArray[3] === boardArray[4] && boardArray[3] === boardArray[5]) {
-        console.log("2nd row win");
+        alert(lastPlayer().name + " wins!");
       }
       // CHECK ROW 3
       if (boardArray[6] === boardArray[7] && boardArray[6] === boardArray[8]) {
-        console.log("3rd row win");
+        alert(lastPlayer().name + " wins!");
       }
       // CHECK COL 1
       if (boardArray[0] === boardArray[3] && boardArray[0] === boardArray[6]) {
-        console.log("1st col win");
+        alert(lastPlayer().name + " wins!");
       }
       // CHECK COL 2
       if (boardArray[1] === boardArray[4] && boardArray[1] === boardArray[7]) {
-        console.log("2nd col win");
+        alert(lastPlayer().name + " wins!");
       }
       // CHECK COL 3
       if (boardArray[2] === boardArray[5] && boardArray[2] === boardArray[8]) {
-        console.log("3rd col win");
+        alert(lastPlayer().name + " wins!");
       }
       // CHECK DIAG \
       if (boardArray[0] === boardArray[4] && boardArray[0] === boardArray[8]) {
-        console.log("\\ diag win");
+        alert(lastPlayer().name + " wins!");
       }
       // CHECK DIAG /
       if (boardArray[2] === boardArray[4] && boardArray[2] === boardArray[6]) {
-        console.log("/ diag win");
+        alert(lastPlayer().name + " wins!");
       }
     }
   }
 
-  // total 8 possible winning combinations
-  function validateRow() {
-    for (var i = 0; i < 3; i++) {
-
+  function resetGame () {
+    console.log("reset button clicked");
+    var len = document.getElementsByClassName("box").length;
+    for (var i = 1; i < len + 1; i++) {
+      var box = "box" + i;
+      document.getElementById(box).classList.remove("x", "o");
     }
+    // RESET EVERYTHING TO START STATE
+    gameStarted = false;
+    round = 1;
+    boardArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    document.getElementById("show-turn").textContent = "Ready for a rematch?";
   }
 
 });
